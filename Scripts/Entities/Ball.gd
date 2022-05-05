5b8b795a7f08955b595e77912005c7be18c34d05
Skengrek <armand.masse@cpe.fr>
2022-05-05 15:44:10 +0200

    Added particule when ball explode

--- Scripts/Entities/Ball.gd
+++ Scripts/Entities/Ball.gd
@@ -5,6 +5,8 @@ var speed = 500
 var direction = Vector2.RIGHT
 var damage = 2
 
+var explosion = preload("res://Scenes/Singletons/explosions.tscn")
+
 
 func _ready():
 	pass
@@ -17,4 +19,10 @@ func _process(delta):
 func _on_Area2D_body_entered(_body):
 	if _body.has_method('hit'):
 		_body.hit(2)
+		var _explosion = explosion.instance()
+		_explosion.global_position = global_position 
+		_explosion.emitting = true
+		_explosion.one_shot = true
+		get_tree().get_root().add_child(_explosion)
 	queue_free()
+
