566c1d1542c852b354c9708103063108eec594a8
Skengrek <armand.masse@cpe.fr>
2022-05-09 18:06:16 +0200

    First work on monster Animation and Behavior

--- Scripts/Entities/Monster.gd
+++ Scripts/Entities/Monster.gd
@@ -4,13 +4,37 @@ extends KinematicBody2D
 onready var lifeBar = $CenterContainer/lifeBar
 var life = 10
 var alive = true
+var rng = RandomNumberGenerator.new()
 
-# Called when the node enters the scene tree for the first time.
+onready var cooldown = $Cooldown
+onready var animPlayer = $AnimationTree
+onready var animState = animPlayer.get('parameters/playback')
+onready var attackRayCast = $RayCasts/attack
+
+var speed = 20
+var nextPosition = Vector2.ZERO
+
 func _ready():
-	pass # Replace with function body.
+	animState.start('Move')
+	nextPosition = position
 
 
 func hit(dmg):
 	life -= dmg
 	if life<=0:
 		queue_free()
+		
+
+func _process(_delta):
+	
+	if nextPosition == global_position:
+		var x = rng.randi_range(5, 15)
+		var y = rng.randi_range(5, 15)
+		print(nextPosition)
+		nextPosition = Vector2(x, y)
+		var success = move_and_slide(nextPosition)
+	
+	if attackRayCast.is_colliding():
+		print('Test')
+		animState.start('Attack')
+	
